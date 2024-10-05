b713a8e208448514b570b14ef1665586fcedf188
user1 <nsai45061@gmail.com>
2024-10-05 13:20:38 +0530

    page indication added to home image

---
+++ lib/models/homeScreenModels/home_image_model.dart
@@ -0,0 +1,24 @@
+import 'package:drfootapp/utils/constants/assets_constants.dart';
+
+class HomeImageModel {
+  String image;
+  HomeImageModel({this.image = ""});
+}
+
+List<HomeImageModel> homeImagesList = [
+  HomeImageModel(
+    image: AssetsConstants.diabetes_image,
+  ),
+  HomeImageModel(
+    image: AssetsConstants.diabetes_image,
+  ),
+  HomeImageModel(
+    image: AssetsConstants.diabetes_image,
+  ),
+  HomeImageModel(
+    image: AssetsConstants.diabetes_image,
+  ),
+  HomeImageModel(
+    image: AssetsConstants.diabetes_image,
+  ),
+];
