142bb54f729e767019a08ff5d40d9c2045b6e65d
nsai45061 <nsai45061@gmail.com>
2025-02-05 13:53:54 +0530

    fixe dissues

--- lib/controllers/records_controller.dart
+++ lib/controllers/records_controller.dart
@@ -1,5 +1,29 @@
+import 'package:drfootapp/utils/constants/constants.dart';
+import 'package:drfootapp/utils/utility.dart';
 import 'package:get/get.dart';
+import 'package:file_picker/file_picker.dart' as filePicker;
 
 class RecordsController extends GetxController {
-  
+  bool isLoading = false;
+
+  void _updateLoading(bool loading) {
+    isLoading = loading;
+    update();
+  }
+
+  uploadRecord() async {
+    try {
+      _updateLoading(true);
+      filePicker.FilePickerResult? result = await filePicker.FilePicker.platform
+          .pickFiles(
+              allowMultiple: false,
+              type: filePicker.FileType.custom,
+              allowedExtensions: ["pdf", "png", "jpg", "jpeg"]);
+    } catch (e) {
+      logger("uploadRecord $e");
+      Utility.toast("failed to upload record $e");
+    } finally {
+      _updateLoading(false);
+    }
+  }
 }
